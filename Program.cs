1f1c5baaacb2ec43afb35223d1ae94cf98929a7c
Thicc-Boi01 <tjc27882@gmail.com>
2021-03-25 16:18:19 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times do you want to repeat this?");
+            string repeatnum  = Console.ReadLine();
+
+
+            int number = int.Parse(repeatnum);
+
+            int counter = 0;
+            
+            while (counter < number) {
+                Console.WriteLine("please enter a name:");
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
+                counter++;
+            }
+            Console.WriteLine("Goodbye :3");
+            
+        }
+    }
+}
